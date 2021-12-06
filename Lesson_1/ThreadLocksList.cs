6fb8cece98ebe09ca222f713a888baa6e9881dda
Vasiliy Pankrukhin <wacpan93@gmail.com>
2021-12-06 15:56:28 +0300

    Решение к дз 1 урока

---
+++ Lesson_1/ThreadLocksList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace Lesson_1
+{
+    public class ThreadLocksList<T> : List<string> 
+    {
+        public void AddFromThread(T Object, int ThreadId)
+        {
+            lock (this)
+            {
+                Add($"Поток {ThreadId} - {Object}\n");
+            }
+        }
+
+        public void RemoveFromThread(int Index)
+        {
+            lock (this)
+            {
+                if (Count >= Index + 1)
+                {
+                    RemoveAt(Index);
+                }
+            }
+        }
+    }
+}
