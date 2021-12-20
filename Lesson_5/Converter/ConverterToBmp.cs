1a99f4f5d7374cb76a60a6ac7bc000e1fa509444
Vasiliy Pankrukhin <wacpan93@gmail.com>
2021-12-20 16:07:39 +0300

    решение к дз 5 урока

---
+++ Lesson_5/Converter/ConverterToBmp.cs
@@ -0,0 +1,15 @@
+﻿namespace Converter
+{
+    public class ConverterToBmp : ConverterLogger, IConverter
+    {
+        public async void Convert(byte[] Value, string FileNameOutput)
+        {
+            var FileName = $"{FileNameOutput}.bmp";
+
+            using (FileStream FileStream = new FileStream(FileName, FileMode.OpenOrCreate))
+            {
+                await FileStream.WriteAsync(Value, 0, Value.Length);
+            }
+        }
+    }
+}
