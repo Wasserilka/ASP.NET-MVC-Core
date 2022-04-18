515c2bace49be88c91738b8882725383cf43fe1a
Vasiliy Pankrukhin <wacpan93@gmail.com>
2022-04-18 17:32:17 +0300

    сдача работы по 9 уроку
    задание из методички + маппинг, данные исходящей почты использовал свои для теста

---
+++ Lesson_8/MapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using Lesson_8.Models;
+using Lesson_8.ViewModels;
+using AutoMapper;
+
+namespace Lesson_8
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Employee, EmployeeViewModel>();
+            CreateMap<EmployeeViewModel, Employee>();
+        }
+    }
+}
---
+++ Lesson_8/Messenger/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace Lesson_8.Messenger.Models
+{
+    public class Message
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string To { get; set; }
+        public string Name { get; set; }
+        public bool IsHtml { get; set; }
+    }
+}
